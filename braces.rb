bfaa576a17e75f35254bccb89a54413a814494af
TRUECALLER <truecaller@TRUECALLERs-MacBook-Air-3.local>
2019-05-20 14:02:26 +0530

    made some space edits

--- braces.rb
+++ braces.rb
@@ -10,17 +10,17 @@ class Braces
     balance_stack = []
     rear = 0
     
-    while(i < @braces_string.length )    
+    while(i < @braces_string.length)    
       if(@braces_string[i] == '{' || @braces_string[i] == '(' || @braces_string[i] == '[')
         balance_stack.push(@braces_string[i])
       else
-          if( balance_stack.length == 0 )
-              return false
-          elsif((@braces_string[i] == '}' && balance_stack[balance_stack.length - 1] == '{') || (@braces_string[i] == ')' && balance_stack[balance_stack.length - 1] == '(') || (@braces_string[i] == ']' && balance_stack[balance_stack.length - 1] == '['))
-              balance_stack.pop()
-          else
-            return false
-          end
+        if(balance_stack.length == 0)
+          return false
+        elsif((@braces_string[i] == '}' && balance_stack[balance_stack.length - 1] == '{') || (@braces_string[i] == ')' && balance_stack[balance_stack.length - 1] == '(') || (@braces_string[i] == ']' && balance_stack[balance_stack.length - 1] == '['))
+          balance_stack.pop()
+        else
+          return false
+        end
       end
       i += 1
     end
